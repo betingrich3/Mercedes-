79adea4495e5a65ea2e2c7e303fa7a56d51bd0ab
Marisel <shenapam3@gmail.com>
2024-12-25 18:10:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,21 +5,56 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/eX51M51.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Bugatti Is Always Active*",
-ANTI_LINK: process.env.ANTI_LINK || "true",
-ANTI_BAD: process.env.ANTI_BAD || "true",
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id 
 PREFIX: process.env.PREFIX || ".",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+// add your prifix for bot
+BOT_NAME: process.env.BOT_NAME || "Mercedes",
+// add bot namw here for menu
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254740007567",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "Marisel",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*Engine By Mercedes*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/De6NL8R.jpeg",
+// add img for alive msg
+LIVE_MSG: process.env.LIVE_MSG || "> *Ooh hey Pal I love you*",
+// add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*Viewed*",
+// set the auto reply massage on status reply    
+MODE: process.env.MODE || "private",
+// make bot public-private-inbox-group 
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+// make true for send automatic voices
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+HEART_REACT: process.env.HEART_REACT || "false",
+// make this true or false for heart reactions only 
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "Bugatti",
-STATUS_REPLY: process.env.STATUS_REPLY || "`😅`",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+// make it true or fasle for only react on owner msg only 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
 };
